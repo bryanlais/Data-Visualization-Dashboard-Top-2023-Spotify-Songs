7fe30be5d9f14bd121d6868bc2b3ed6c957cdbd0
Bryan Lai <bryanjlais@gmail.com>
2023-12-07 16:27:38 -0500

    Add area chart

--- data_generation/newcsv.py
+++ data_generation/newcsv.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-source_csv = pd.read_csv('9x9.csv')
-destination_csv = pd.read_csv('mds_coordinates.csv')
-columns_to_add = ["released_day","bpm","danceability_%","valence_%","energy_%","acousticness_%","liveness_%","speechiness_%"]
+source_csv = pd.read_csv('spotify-2023.csv')
+destination_csv = pd.read_csv('9x9.csv')
+columns_to_add = ["released_month"]
 for i in columns_to_add:
     destination_csv[i] = source_csv[i]
-destination_csv.to_csv('mds_coordinates.csv', index=False)
+destination_csv.to_csv('10x10.csv', index=False)
